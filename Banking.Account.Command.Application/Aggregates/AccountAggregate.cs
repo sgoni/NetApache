7a371508281b5c5fe188ed529349bf38f147d2e8
s.goni <steven.goni@outlook.com>
2024-04-18 11:51:31 -0600

    Crear eventos en aggregate.

--- Banking.Account.Command.Application/Aggregates/AccountAggregate.cs
+++ Banking.Account.Command.Application/Aggregates/AccountAggregate.cs
@@ -1,12 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Banking.Account.Command.Application.Features.BankAccount.Commands.DepositFund;
+using Banking.Account.Command.Application.Features.BankAccount.Commands.OpenAccount;
+using Banking.CQRS.Core.Domain;
+using Banking.CQRS.Core.Events;
+using static MongoDB.Driver.WriteConcern;
 
 namespace Banking.Account.Command.Application.Aggregates
 {
-    public class AccountAggregate
+    public class AccountAggregate : AggregateRoot
     {
+        public bool Active { get; set; }
+        public double Balance { get; set; }
+
+        public AccountAggregate() { }
+
+        public AccountAggregate(OpenAccountCommand command)
+        {
+            var accountOpenedEvent = new AccountOpenedEvent(
+                command.Id,
+                command.AccountHolder,
+                command.AccountType,
+                DateTime.Now,
+                command.OpeningBalance
+                );
+
+            RaiseEvent(accountOpenedEvent);
+        }
+
+        public void Apply(AccountOpenedEvent @event)
+        {
+            Id = @event.Id;
+            Active = true;
+            Balance = @event.OpeningBalance;
+        }
+
+        public void DepositFunds(double amount)
+        {
+            if (!Active)
+            {
+                throw new Exception("Los fondos no pueden ser depositados en una cuenta cancelada.");
+            }
+
+            if (amount <= 0)
+            {
+                throw new Exception("El deposito de dinero debe de ser mayor que cero.");
+            }
+
+            var fundsDepositevent = new FundsDepositedEvent(Id)
+            {
+                Id = Id,
+                Amount = amount,
+            };
+
+            RaiseEvent(fundsDepositevent);
+        }
+
+        public void Apply(FundsDepositedEvent @event)
+        {
+            Id = @event.Id;
+            Balance = @event.Amount;
+        }
+
+        public void WithdrawFunds(double amount)
+        {
+            if (!Active)
+            {
+                throw new Exception("La cuenta bancaria esta cerrada.");
+            }
+
+            var fundsWithDrawnEvent = new FundsWithdrawnEvent(Id)
+            {
+                Id = Id,
+                Amount = amount,
+            };
+
+            RaiseEvent(fundsWithDrawnEvent);
+        }
+
+        public void Apply(FundsWithdrawnEvent @event)
+        {
+            Id = @event.Id;
+            Balance = @event.Amount;
+        }
+
+        public void CloseAccount()
+        {
+            if (!Active)
+            {
+                throw new Exception("La cuenta bancaria esta cerrada.");
+            }
+
+            var closeAccountEvent = new AccountClosedEvent(Id);
+            RaiseEvent(closeAccountEvent);
+        }
+
+        public void Apply(AccountClosedEvent @event)
+        {
+            Id = @event.Id;
+        }
     }
 }
---
+++ Banking.Account.Command.Application/Features/BankAccount/Commands/WithdrawnFund/WithdrawFundsCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Banking.Account.Command.Application.Features.BankAccount.Commands.WithdrawnFund
+{
+    public  class WithdrawFundsCommand : IRequest<bool>
+    {
+        public string Id { get; set; } = string.Empty;
+        public double Amount { get; set; }
+    }
+}
